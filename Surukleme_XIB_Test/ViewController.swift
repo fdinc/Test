dcd77932f1c0537672757958991364eca437700e
Fatih Dinç <fatihdinc@fatihs-macbook-pro.local>
2017-02-22 15:20:10 +0200

    Initial Commit

---
+++ Surukleme_XIB_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Surukleme_XIB_Test
+//
+//  Created by Fatih Dinç on 22/02/17.
+//  Copyright © 2017 Fatih Dinç. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
